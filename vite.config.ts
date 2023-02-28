20cd1a5b53a5cfa2281ebbf972285781377a8d88
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-02-28 08:42:25 +0100

    Added react-icons and font

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePluginFonts } from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePluginFonts({
+			google: {
+				families: [
+					{
+						/**
+						 * Family name (required)
+						 */
+						name: "IBM Plex Mono",
+
+						/**
+						 * Family styles
+						 */
+						styles: "wght@400",
+
+						/**
+						 * enable non-blocking renderer
+						 *   <link rel="preload" href="xxx" as="style" onload="this.rel='stylesheet'">
+						 * default: true
+						 */
+						defer: true,
+					},
+				],
+			},
+		}),
+	],
 });
