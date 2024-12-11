e73a59dfa4ad0c9be1264b363ad6126a8d317e3b
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-12-11 20:41:06 +0100

    feat: add x icon, update to vite 5 and refactor footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,50 +1,59 @@
 import { AiFillInstagram } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import { FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
 import { SiHashnode } from "react-icons/si";
-import { VscGithubInverted, VscTwitter } from "react-icons/vsc";
+import { VscGithubInverted } from "react-icons/vsc";
 
 import "./Footer.scss";
+
+const socialLinks = [
+    {
+        href: "https://github.com/jolle11",
+        label: "Link to my github repository",
+        icon: <VscGithubInverted />,
+    },
+    {
+        href: "https://www.linkedin.com/in/jordi-oll%C3%A9-ballest%C3%A9-8398b181/",
+        label: "Link to my linkedin profile",
+        icon: <FaLinkedinIn />,
+        className: "footer__link--linkedin",
+    },
+    {
+        href: "https://jordi0lle.hashnode.dev/",
+        label: "Link to my hashnode blog",
+        icon: <SiHashnode />,
+        className: "footer__link--hashnode",
+    },
+    {
+        href: "https://twitter.com/jordi0lle",
+        label: "Link to my X (formerly Twitter) profile",
+        icon: <FaXTwitter />,
+        className: "footer__link--twitter",
+    },
+    {
+        href: "https://www.instagram.com/jordi0lle/",
+        label: "Link to my instagram profile",
+        icon: <AiFillInstagram />,
+        className: "footer__link--instagram",
+    },
+];
 
 const Footer = () => {
     return (
         <div className="footer">
             <p className="footer__text">Created by Jordi Ollé</p>
             <div className="footer__links">
-                <a
-                    href="https://github.com/jolle11"
-                    className="footer__link"
-                    aria-label="Link to my github repository"
-                >
-                    <VscGithubInverted />
-                </a>
-                <a
-                    href="https://www.linkedin.com/in/jordi-oll%C3%A9-ballest%C3%A9-8398b181/"
-                    className="footer__link footer__link--linkedin"
-                    aria-label="Link to my linkedin profile"
-                >
-                    <FaLinkedinIn />
-                </a>
-                <a
-                    href="https://jordi0lle.hashnode.dev/"
-                    className="footer__link footer__link--hashnode"
-                    aria-label="Link to my hashnode blog"
-                >
-                    <SiHashnode />
-                </a>
-                <a
-                    href="https://twitter.com/jordi0lle"
-                    className="footer__link footer__link--twitter"
-                    aria-label="Link to my twitter profile"
-                >
-                    <VscTwitter />
-                </a>
-                <a
-                    href="https://www.instagram.com/jordi0lle/"
-                    className="footer__link footer__link--instagram"
-                    aria-label="Link to my instagram profile"
-                >
-                    <AiFillInstagram />
-                </a>
+                {socialLinks.map(({ href, label, icon, className = "" }) => (
+                    <a
+                        key={href}
+                        href={href}
+                        className={`footer__link ${className}`}
+                        aria-label={label}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {icon}
+                    </a>
+                ))}
             </div>
         </div>
     );
